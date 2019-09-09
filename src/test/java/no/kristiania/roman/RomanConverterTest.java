f57f922ce5dfd43baa6a640eba9788a1731d3871
Ole <olenorholm55@gmail.com>
2019-09-09 21:49:31 +0200

    added a few more tests!

--- src/test/java/no/kristiania/roman/RomanConverterTest.java
+++ src/test/java/no/kristiania/roman/RomanConverterTest.java
@@ -20,15 +20,45 @@ public class RomanConverterTest {
         assertEquals("III", toRoman(3));
 
     }
+    @Test
+    void shouldReturnIVFor4() {
+        assertEquals("IV", toRoman(4));
+
+    }
+    @Test
+    void shouldReturnVFor5() {
+        assertEquals("V", toRoman(5));
+
+    }
+    @Test
+    void shouldCalcualte6through8() {
+        assertEquals("VI", toRoman(6));
+        assertEquals("VII", toRoman(7));
+        assertEquals("VIII", toRoman(8));
+
+    }
+    @Test
+    void shouldReturnIXFor9() {
+        assertEquals("IX", toRoman(9));
+
+    }
     private String toRoman(int number) {
-        String result = "";
-        if (number == 2) {
-            result = "II";
-        } else if (number == 1) {
-            result = "I";
-        }else if (number == 3) {
-            result = "III";
+        StringBuilder result = new StringBuilder();
+        if (number == 9) {
+            result.append("IX");
+            number = 0;
         }
-        return result;
+        if (number >= 5) {
+            result.append("V");
+            number -= 5;
+        }
+        if (number == 4) {
+            result.append("IV");
+            number = 0;
+        }
+        for (int i = 0; i < number; i++) { // checks 1-3
+            result.append("I");
+        }
+        return result.toString();
     }
 }
