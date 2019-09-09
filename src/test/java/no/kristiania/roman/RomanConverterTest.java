815ed3a80e76a74d28e6aa8df395717ddd44aba0
Ole <olenorholm55@gmail.com>
2019-09-09 21:36:07 +0200

    added III = 3 test

--- src/test/java/no/kristiania/roman/RomanConverterTest.java
+++ src/test/java/no/kristiania/roman/RomanConverterTest.java
@@ -1,5 +1,34 @@
 package no.kristiania.roman;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanConverterTest {
-// test
+    @Test
+    void shouldReturnIFor1() {
+        assertEquals("I", toRoman(1));
+
+    }
+    @Test
+    void shouldReturnIIFor1() {
+        assertEquals("II", toRoman(2));
+
+    }
+    @Test
+    void shouldReturnIIIFor3() {
+        assertEquals("III", toRoman(3));
+
+    }
+    private String toRoman(int number) {
+        String result = "";
+        if (number == 2) {
+            result = "II";
+        } else if (number == 1) {
+            result = "I";
+        }else if (number == 3) {
+            result = "III";
+        }
+        return result;
+    }
 }
