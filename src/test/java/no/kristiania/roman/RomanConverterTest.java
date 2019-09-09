1af9a677fa063aa76691ae6d8400a755550f6381
Ole <olenorholm55@gmail.com>
2019-09-10 00:25:03 +0200

    in case something get fricked up

--- src/test/java/no/kristiania/roman/RomanConverterTest.java
+++ src/test/java/no/kristiania/roman/RomanConverterTest.java
@@ -7,70 +7,56 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanConverterTest {
     @Test
     void shouldReturnIFor1() {
-        assertEquals("I", toRoman(1));
+        assertEquals("I", RomanConverter.toRoman(1));
 
     }
     @Test
-    void shouldReturnIIFor1() {
-        assertEquals("II", toRoman(2));
+    void shouldReturnIIFor2() {
+        assertEquals("II", RomanConverter.toRoman(2));
 
     }
     @Test
     void shouldReturnIIIFor3() {
-        assertEquals("III", toRoman(3));
+        assertEquals("III", RomanConverter.toRoman(3));
 
     }
     @Test
     void shouldReturnIVFor4() {
-        assertEquals("IV", toRoman(4));
+        assertEquals("IV", RomanConverter.toRoman(4));
 
     }
     @Test
     void shouldReturnVFor5() {
-        assertEquals("V", toRoman(5));
+        assertEquals("V", RomanConverter.toRoman(5));
 
     }
     @Test
     void shouldCalcualte6through8() {
-        assertEquals("VI", toRoman(6));
-        assertEquals("VII", toRoman(7));
-        assertEquals("VIII", toRoman(8));
+        assertEquals("VI", RomanConverter.toRoman(6));
+        assertEquals("VII", RomanConverter.toRoman(7));
+        assertEquals("VIII", RomanConverter.toRoman(8));
 
     }
     @Test
     void shouldReturnIXFor9() {
-        assertEquals("IX", toRoman(9));
+        assertEquals("IX", RomanConverter.toRoman(9));
 
     }
     @Test
     void shouldCalculateNumbersOver10() {
-        assertEquals("X", toRoman(10));
-        assertEquals("XVIII", toRoman(18));
+        assertEquals("X", RomanConverter.toRoman(10));
+        assertEquals("XVIII", RomanConverter.toRoman(18));
 
     }
-    private String toRoman(int number) {
-        StringBuilder result = new StringBuilder();
-        number = toRomanDigit(number, result, 10, "X");
-        if (number == 9) {
-            result.append("IX");
-            number = 0;
-        }
-        number = toRomanDigit(number, result, 5, "V");
-        if (number == 4) {
-            result.append("IV");
-            number = 0;
-        }
-        for (int i = 0; i < number; i++) { // checks 1-3
-            result.append("I");
-        }
-        return result.toString();
+
+    @Test
+    void shouldCalculateMultiplesOfTen() {
+        assertEquals("XXXVII", RomanConverter.toRoman(37));
     }
 
-    private int toRomanDigit(int number, StringBuilder result, int digitValue, String digitSymbol) {
-        if (number >= digitValue) {
-            result.append(digitSymbol);
-            number -= digitValue;
-        }
-        return number;
+    @Test
+    void shouldCalculateSimpleDigits() {
+        assertEquals("MMMDCCLXXXVIII", RomanConverter.toRoman(3888));
     }
+
 }
